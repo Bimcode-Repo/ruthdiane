7921a8f01290a6e6b6a79523746348ec06dc4ffe
root <root@websource.fr>
2025-11-20 09:51:55 +0000

    Update intro banner padding on mobile

--- resources/views/pages/partials/about/intro-banner.blade.php
+++ resources/views/pages/partials/about/intro-banner.blade.php
@@ -1,5 +1,5 @@
-<div class="h-[30vh] bg-primary flex items-center justify-center px-8 md:px-16">
-    <p class="text-background text-2xl text-white text-center max-w-4xl font-joan" data-aos="zoom-in">
+<div class="bg-primary flex items-center justify-center px-8 md:px-16 py-16">
+    <p class="text-background text-2xl text-white text-center max-w-4xl my-16 font-joan" data-aos="zoom-in">
         {{ __('messages.lorem_medium') }}
     </p>
 </div>
